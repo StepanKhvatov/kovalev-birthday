8daf0f26a331eb66e1a13fba4f59b6ad2cdb73e3
StepanTeshka <leluhinstepan@gmail.com>
2022-08-21 02:10:07 +0300

    sport and fix pirat

--- src/components/Sport.jsx
+++ src/components/Sport.jsx
@@ -1,14 +1,27 @@
+import tigerImage from "../images/tiger.png"
 import sportStyles from "../styles/Sport.module.css";
+import pumpItImage from "../images/pumpIt.png"
+import sportVideo from "../video/2022-08-21 01.43.49.mp4"
 
 const Sport = () => {
   return (
-    <section>
+    <section className={sportStyles.section}>
       <div className="container">
         <div className={sportStyles.content}>
           <div className={sportStyles["title-container"]}>
             <h2 className={`${sportStyles.title} title`}>успешный спортсмен</h2>
+            <p className={sportStyles.description} >Все успешные спортсмены целеустремленные </p>
           </div>
-          <div></div>
+          <div className={sportStyles["content-container"]}>
+            <div className={sportStyles["video-container"]}>
+              <iframe className={sportStyles["content-block"]} src={sportVideo} title="sport"/>
+              <img src={tigerImage} alt="tiger" className={sportStyles.tiger}/>
+            </div>
+            <p className={sportStyles.vs}>
+              VS
+            </p>
+              <img src={pumpItImage} alt="pump" className={sportStyles["content-block"]}/>
+          </div>
         </div>
       </div>
     </section>
