78d251f478e3374d6629c40528b30361312cc463
Stepan <stepanhv@yandex.ru>
2022-08-21 14:07:13 +0300

    Create Father component

--- src/components/Father.jsx
+++ src/components/Father.jsx
@@ -1,10 +1,23 @@
+import father from "../images/father.jpg";
+import baboon from "../images/baboon.png";
 import fatherStyles from "../styles/Father.module.css";
 
 const Father = () => {
   return (
     <section className={fatherStyles.section}>
+      <img src={baboon} alt="baboon" className={fatherStyles.baboon} />
       <div className="container">
-        <h2 className={`${fatherStyles.title} title`}>молодой отец</h2>
+        <div className={fatherStyles.content}>
+          <div className={fatherStyles["image-container"]}>
+            <img src={father} alt="father" className={fatherStyles.image} />
+          </div>
+          <div className={fatherStyles["title-container"]}>
+            <h2 className={`${fatherStyles.title} title`}>молодой отец</h2>
+            <p className={fatherStyles.subtitle}>
+              Молодые мамы хороши. Молодые отцы тоже, знаете ли, ничего ;)
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
