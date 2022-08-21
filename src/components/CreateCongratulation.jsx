947573767c8af10508a81203b8fba4776cae9a3e
Stepan <stepanhv@yandex.ru>
2022-08-21 11:42:34 +0300

    Added colors to form section

--- src/components/CreateCongratulation.jsx
+++ src/components/CreateCongratulation.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import CongratulationForm from "./CongratulationForm";
 
 const CreateCongratulation = ({ setData }) => {
+  const [selectedColor, setSelectedColor] = useState("#FFF960");
+
   return (
-    <section style={{ background: "#f2eb3f" }}>
+    <section style={{ background: selectedColor }}>
       <div className="container">
-        <CongratulationForm setData={setData} />
+        <CongratulationForm
+          setData={setData}
+          selectedColor={selectedColor}
+          setSelectedColor={setSelectedColor}
+        />
       </div>
     </section>
   );
