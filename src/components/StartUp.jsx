acf0c6539833bf4f5bba1aa9a41b68ef85edcc37
Stepan <stepanhv@yandex.ru>
2022-08-21 16:46:12 +0300

    Added melon image

--- src/components/StartUp.jsx
+++ src/components/StartUp.jsx
@@ -3,6 +3,7 @@ import MediaGallery from "./MediaGallery";
 import Zenfuse from "../images/zenfuse.jpg";
 import Dexsport from "../images/dexsport.jpg";
 import Subclub from "../images/subclub.jpg";
+import melon from "../images/melon.png";
 import { useRef } from "react";
 import { useInView } from "framer-motion";
 
@@ -12,7 +13,8 @@ const StartUp = () => {
 
   return (
     <section className={startUpStyles.section}>
-      <div className="container">
+      <div style={{ position: "relative" }} className="container">
+        <img className={startUpStyles.melon} src={melon} alt="melon" />
         <h2
           style={{
             transform: isInView ? "none" : "translateY(-40px)",
